d2a7a4e494b3bebc561be0611225b0d337badafb
Ilya Kirbizhekov <ilyoniss@mail.ru>
2023-10-10 13:52:22 +1100

    initial commit

---
+++ src/main/java/com/javarush/domain/Film.java
@@ -0,0 +1,22 @@
+package com.javarush.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(schema = "movie", name = "film")
+public class Film {
+    @Id
+    @Column(name = "film_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Short id;
+
+    private String title;
+
+    @Column(columnDefinition = "text")
+    @Type(type = "text")
+    private String description;
+
+
+
+}
